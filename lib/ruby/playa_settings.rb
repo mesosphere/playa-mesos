653ef4cf723ac5959685eb9e58b2ffa97d4e2d43
Jeremy Lingmann <jeremy@mesosphere.io>
2014-01-12 18:28:36 -0800

    Add support for Packer box image builds
    The Mesos box image can now be built with Packer and is also available
    for download from AWS S3.

--- lib/ruby/playa_settings.rb
+++ lib/ruby/playa_settings.rb
@@ -26,6 +26,38 @@ class PlayaSettings
     File.dirname(settings_file)
   end
 
+  # Return the path of the packer builds
+  def build_path
+    File.join(settings_path, 'packer', 'builds')
+  end
+
+  # Aborts (exit with failure code) if URI is invalid
+  def box_url
+    url = @settings['box_url']
+    begin
+      URI(url) if url
+    rescue URI::Error => e
+      abort "Malformed URL in #{settings_file}\n#{e}"
+    end
+    url
+  end
+
+  # Return the last part of the box_url path (the box filename)
+  def box_filename
+    URI(box_url).path.split('/').last
+  end
+
+  # Return the local filesystem path of the box specified by #box_url if it can
+  # be found locally, nil otherwise.
+  def box_local
+    result = nil
+    if box_url
+      test_path = File.join(build_path, box_filename)
+      result = test_path if File.readable?(test_path)
+    end
+    result
+  end
+
   private
 
   def read_user_settings(settings_file)
